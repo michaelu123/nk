b765f0274ff43ba95c5845a86b07b663f3ea33cb
Michael Uhlenberg <michael.uhlenberg@t-online.de>
2025-01-06 21:55:42 +0100

    mkcert,favicon

--- src/routes/nk/[id]/+page.ts
+++ src/routes/nk/[id]/+page.ts
@@ -1,7 +1,7 @@
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ params }) => {
+export const load: PageLoad = async ({ params, url }) => {
 	const { id } = params;
 	console.log('ID', id);
-	return { id };
+	return { id, url };
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: { host: '0.0.0.0', port: 5173 }
+	plugins: [sveltekit(), mkcert()],
+	server: { host: '0.0.0.0' }
 });
